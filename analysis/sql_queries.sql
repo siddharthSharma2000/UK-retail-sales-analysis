c5e4c6c6d2ad2a231c786c8a22682b4851850b26
siddharthSharma2000 <siddharthsharma29042000@gmail.com>
2025-12-26 20:18:08 +0000

    sql_queries.sql

--- analysis/sql_queries.sql
+++ analysis/sql_queries.sql
@@ -1,1 +1,21 @@
+SELECT 
+    Region, 
+    SUM(Sales_Value) AS Total_Revenue,
+    COUNT(Transaction_ID) AS Transaction_Count
+FROM retail_sales
+GROUP BY Region
+ORDER BY Total_Revenue DESC;
 
+SELECT 
+    Day_Type, -- (Created during data cleaning)
+    AVG(Sales_Value) AS Avg_Transaction_Value,
+    SUM(Sales_Value) AS Total_Revenue
+FROM retail_sales
+GROUP BY Day_Type;
+
+SELECT TOP 5
+    Product_Category, 
+    SUM(Quantity_Sold) AS Total_Items_Sold
+FROM retail_sales
+GROUP BY Product_Category
+ORDER BY Total_Items_Sold DESC;
